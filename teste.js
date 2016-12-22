97d55fad3dd9f937b338a6416be8ad527b599c97
Denner Evaldt <dnrevaldt@gmail.com>
2016-12-22 11:19:06 -0200

    Refatoring

--- teste.js
+++ teste.js
@@ -25,4 +25,15 @@ app.get('/clients', function (req, res) {
   res.json({'msg': 'Hello Clients'});
 });
 
+// error handling
+app.use(function(request, response, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, request, response, next) {
+	response.status(err.status || 500).json({ error: err.errors || err.message });
+});
+
 app.listen(3000);
